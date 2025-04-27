3c29264006b4f5f91c617eca9b8e965a852a3c6e
cetaylor07 <cetaylor07@gmail.com>
2025-04-27 13:07:04 -0400

    updated file location for logging file for networth_snapshot

--- take_networth_snapshot.py
+++ take_networth_snapshot.py
@@ -10,7 +10,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler("net_worth_snapshot.log"),
+        logging.FileHandler("./logs/net_worth_snapshot.log"),
         logging.StreamHandler(sys.stdout)
     ]
 )
